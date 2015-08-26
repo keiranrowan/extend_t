d8eb671d6ad67070b07ccf8c07fcbc3665a7af50
Harold H. Kibb <hhkibb@airmail.cc>
2015-08-26 09:52:25 -0400

    Added some security features

--- array/array.c
+++ array/array.c
@@ -5,8 +5,8 @@
 void array_init(array_t *array, GENERIC template)
 {
 	array->size = 0;
-    array->capacity = 4;
-    array->template = template;
+	array->capacity = 4;
+	array->template = template;
 	array->cargo = malloc(sizeof(template) * array->capacity);
 }
 
@@ -14,14 +14,18 @@ void array_append(array_t *array, GENERIC value)
 {
     array_update_capacity(array);
     
-    array->cargo[array->size++] = value;
+    if (sizeof(value) == sizeof(array->template)) {
+    	array->cargo[array->size++] = value;
+    } else {
+    	printf("Size Mismatch: Trying to pack var of size %zd into slot of size %zd", sizeof(value), sizeof(array->template));
+    }
 }
 
 GENERIC array_get(array_t *array, int index)
 {
     if (index >= array->size || index < 0) {
-        printf("Index out of Bounds. Size: %d Attempted Index: %d\n", array->size, index);
-        exit(1);
+        fprintf(stderr, "Index out of Bounds. Size: %d Attempted Index: %d\n", array->size, index);
+        return;
     }
     return array->cargo[index];
 }
@@ -31,7 +35,11 @@ void array_set(array_t *array, int index, GENERIC value)
     while (index >= array->size) {
         array_append(array, NULL);
     }
-    array->cargo[index] = value;
+    if (sizeof(value) == sizeof(array->template)) {
+    	array->cargo[index] = value;
+    } else {
+    	fprintf(stderr, "Size Mismatch: Trying to pack var of size %zd into slot of size %zd", sizeof(value), sizeof(array->template));
+    }
 }
 
 void array_update_capacity(array_t *array)
@@ -39,6 +47,9 @@ void array_update_capacity(array_t *array)
     if (array->size >= array->capacity) {
         array->capacity *= 2;
         array->cargo = realloc(array->cargo, sizeof(array->template) * array->capacity);
+        if (array->cargo == NULL) {
+        	fprintf(stderr, "Memory Error: Unable to reallocate array")
+        }
     }
 }
 
